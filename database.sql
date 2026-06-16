71cadaf724cef9dba8692a70d2e1738fa3df567b
Kauã Brito <auak0101@gmail.com>
2026-06-16 19:25:39 -0300

    add database.php

---
+++ database.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, email, password) VALUES ('João Silva', 'joao@example',
+'senha123');
+INSERT INTO users (name, email, password) VALUES ('Maria Souza', 'maria@example',
+'senha456');        
